a57927f3c7bf700f3c7ed399185a8d471d8c2fe5
huahungnguyen121 <63572298+huahungnguyen121@users.noreply.github.com>
2022-08-18 16:44:10 +0700

    API leaflet map search

--- src/views/MapView/LeafletMaps/LeafletMapsView.vue
+++ src/views/MapView/LeafletMaps/LeafletMapsView.vue
@@ -11,34 +11,58 @@
 </template>
 <script>
 import Leaflet from "leaflet";
+import { geocoder } from "leaflet-control-geocoder";
 
 export default {
+    data() {
+        return {
+            map: null,
+            marker: null,
+            lat: 10.762622,
+            long: 106.660172,
+            zoomLevel: 15,
+        };
+    },
     mounted() {
         Leaflet.Icon.Default.imagePath = import.meta.env.BASE_URL;
-        const map = Leaflet.map("map").setView([10.762622, 106.660172], 15);
+        this.map = Leaflet.map("map").setView(
+            [this.lat, this.long],
+            this.zoomLevel
+        );
 
         Leaflet.tileLayer("https://tile.osm.org/{z}/{x}/{y}.png", {
             maxZoom: 19,
             attribution:
                 '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-        }).addTo(map);
+        }).addTo(this.map);
 
-        let marker;
+        const geoCoder = geocoder({
+            defaultMarkGeocode: false,
+        }).addTo(this.map);
 
-        map.on("click", (e) => {
-            if (e.latlng) {
-                if (!marker) {
-                    marker = Leaflet.marker([e.latlng.lat, e.latlng.lng])
-                        .addTo(map)
-                        .bindPopup(
-                            "A pretty CSS3 popup.<br> Easily customizable."
-                        )
-                        .openPopup();
-                    return;
-                }
-                marker.setLatLng([e.latlng.lat, e.latlng.lng]).update();
+        geoCoder.addEventListener("markgeocode", (e) => {
+            if (e) {
+                this.setMarker(e.geocode.center.lat, e.geocode.center.lng);
             }
         });
+
+        this.map.on("click", (e) => {
+            if (e.latlng) {
+                this.setMarker(e.latlng.lat, e.latlng.lng);
+            }
+        });
+    },
+    methods: {
+        setMarker(lat, long) {
+            if (!this.marker) {
+                this.marker = Leaflet.marker([lat, long])
+                    .addTo(this.map)
+                    .openPopup();
+                return;
+            }
+            this.marker.setLatLng([lat, long]).update();
+            this.map.setView([lat, long], 17);
+        },
     },
 };
 </script>
